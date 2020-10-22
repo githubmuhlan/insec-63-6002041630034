dfb3d26da49d51f7403739627a052fdaeecd2e80
muhlan <s6002041630034@email.kmutnb.ac.th>
2020-10-22 19:32:26 +0700

    crate migration permission_to_role

---
+++ console/migrations/m201022_113458_create_post_permission_to_role.php
@@ -0,0 +1,84 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201022_113458_create_post_permission_to_role
+ */
+class m201022_113458_create_post_permission_to_role extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $auth = Yii::$app->authManager;
+
+        $author = $auth->getRole('author');
+        $admin = $auth->getRole('admin');
+        $superAdmin = $auth->getRole('super-admin');
+
+        $listPost = $auth->getPermission('post-index');
+        $createPost = $auth->getPermission('post-create');
+        $updatePost = $auth->getPermission('post-update');
+        $viewPost = $auth->getPermission('post-view');
+        $deletePost = $auth->getPermission('post-delete');
+
+        $auth->addChild($author, $createPost);
+        $auth->addChild($author, $listPost);
+        $auth->addChild($author, $updatePost);
+        $auth->addChild($author, $viewPost);
+
+        $auth->addChild($admin, $author);
+        
+        $auth->addChild($superAdmin, $admin);
+        $auth->addChild($superAdmin, $deletePost);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        //echo "m201022_113458_create_post_permission_to_role cannot be reverted.\n"
+
+        $auth = Yii::$app->authManager;
+
+        $author = $auth->getRole('author');
+        $admin = $auth->getRole('admin');
+        $superAdmin = $auth->getRole('super-admin');
+
+        $listPost = $auth->getPermission('post-index');
+        $createPost = $auth->getPermission('post-create');
+        $updatePost = $auth->getPermission('post-update');
+        $viewPost = $auth->getPermission('post-view');
+        $deletePost = $auth->getPermission('post-delete');
+
+        $auth->removeChild($author, $createPost);
+        $auth->removeChild($author, $listPost);
+        $auth->removeChild($author, $updatePost);
+        $auth->removeChild($author, $viewPost);
+
+        $auth->removeChild($admin, $author);
+        
+        $auth->removeChild($superAdmin, $admin);
+        $auth->removeChild($superAdmin, $deletePost);
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201022_113458_create_post_permission_to_role cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
